0805200f3fcd42038f1750d21547340da69ee60b
CodeSenpai <finnmeiberg@gmail.com>
2021-05-13 21:24:56 +0200

    Projektdateien hinzufügen.

---
+++ Game.Designer.cs
@@ -0,0 +1,331 @@
+﻿
+namespace Tic_Tac_Toe_Game
+{
+    partial class Game
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn11 = new System.Windows.Forms.Button();
+            this.btn12 = new System.Windows.Forms.Button();
+            this.btn13 = new System.Windows.Forms.Button();
+            this.btn21 = new System.Windows.Forms.Button();
+            this.btn22 = new System.Windows.Forms.Button();
+            this.btn23 = new System.Windows.Forms.Button();
+            this.btn31 = new System.Windows.Forms.Button();
+            this.btn32 = new System.Windows.Forms.Button();
+            this.btn33 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.resetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblwinner = new System.Windows.Forms.Label();
+            this.lblp1 = new System.Windows.Forms.Label();
+            this.lblp2 = new System.Windows.Forms.Label();
+            this.lblwp1 = new System.Windows.Forms.Label();
+            this.lblwp2 = new System.Windows.Forms.Label();
+            this.restartToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn11
+            // 
+            this.btn11.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btn11.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btn11.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn11.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.btn11.Location = new System.Drawing.Point(24, 150);
+            this.btn11.Name = "btn11";
+            this.btn11.Size = new System.Drawing.Size(50, 50);
+            this.btn11.TabIndex = 0;
+            this.btn11.Text = "   ";
+            this.btn11.UseMnemonic = false;
+            this.btn11.UseVisualStyleBackColor = false;
+            this.btn11.Click += new System.EventHandler(this.btn11_Click);
+            // 
+            // btn12
+            // 
+            this.btn12.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btn12.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btn12.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn12.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.btn12.Location = new System.Drawing.Point(80, 150);
+            this.btn12.Name = "btn12";
+            this.btn12.Size = new System.Drawing.Size(50, 50);
+            this.btn12.TabIndex = 1;
+            this.btn12.Text = "   ";
+            this.btn12.UseMnemonic = false;
+            this.btn12.UseVisualStyleBackColor = false;
+            this.btn12.Click += new System.EventHandler(this.btn12_Click);
+            // 
+            // btn13
+            // 
+            this.btn13.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btn13.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btn13.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn13.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.btn13.Location = new System.Drawing.Point(136, 150);
+            this.btn13.Name = "btn13";
+            this.btn13.Size = new System.Drawing.Size(50, 50);
+            this.btn13.TabIndex = 2;
+            this.btn13.Text = "   ";
+            this.btn13.UseMnemonic = false;
+            this.btn13.UseVisualStyleBackColor = false;
+            this.btn13.Click += new System.EventHandler(this.btn13_Click);
+            // 
+            // btn21
+            // 
+            this.btn21.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btn21.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btn21.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn21.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.btn21.Location = new System.Drawing.Point(24, 206);
+            this.btn21.Name = "btn21";
+            this.btn21.Size = new System.Drawing.Size(50, 50);
+            this.btn21.TabIndex = 3;
+            this.btn21.Text = "   ";
+            this.btn21.UseMnemonic = false;
+            this.btn21.UseVisualStyleBackColor = false;
+            this.btn21.Click += new System.EventHandler(this.btn21_Click);
+            // 
+            // btn22
+            // 
+            this.btn22.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btn22.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btn22.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn22.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.btn22.Location = new System.Drawing.Point(80, 206);
+            this.btn22.Name = "btn22";
+            this.btn22.Size = new System.Drawing.Size(50, 50);
+            this.btn22.TabIndex = 4;
+            this.btn22.Text = "   ";
+            this.btn22.UseMnemonic = false;
+            this.btn22.UseVisualStyleBackColor = false;
+            this.btn22.Click += new System.EventHandler(this.btn22_Click);
+            // 
+            // btn23
+            // 
+            this.btn23.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btn23.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btn23.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn23.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.btn23.Location = new System.Drawing.Point(136, 206);
+            this.btn23.Name = "btn23";
+            this.btn23.Size = new System.Drawing.Size(50, 50);
+            this.btn23.TabIndex = 5;
+            this.btn23.Text = "   ";
+            this.btn23.UseMnemonic = false;
+            this.btn23.UseVisualStyleBackColor = false;
+            this.btn23.Click += new System.EventHandler(this.btn23_Click);
+            // 
+            // btn31
+            // 
+            this.btn31.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btn31.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btn31.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn31.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.btn31.Location = new System.Drawing.Point(24, 262);
+            this.btn31.Name = "btn31";
+            this.btn31.Size = new System.Drawing.Size(50, 50);
+            this.btn31.TabIndex = 6;
+            this.btn31.Text = "   ";
+            this.btn31.UseMnemonic = false;
+            this.btn31.UseVisualStyleBackColor = false;
+            this.btn31.Click += new System.EventHandler(this.btn31_Click);
+            // 
+            // btn32
+            // 
+            this.btn32.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btn32.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btn32.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn32.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.btn32.Location = new System.Drawing.Point(80, 262);
+            this.btn32.Name = "btn32";
+            this.btn32.Size = new System.Drawing.Size(50, 50);
+            this.btn32.TabIndex = 7;
+            this.btn32.Text = "   ";
+            this.btn32.UseMnemonic = false;
+            this.btn32.UseVisualStyleBackColor = false;
+            this.btn32.Click += new System.EventHandler(this.btn32_Click);
+            // 
+            // btn33
+            // 
+            this.btn33.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btn33.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btn33.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn33.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.btn33.Location = new System.Drawing.Point(136, 262);
+            this.btn33.Name = "btn33";
+            this.btn33.Size = new System.Drawing.Size(50, 50);
+            this.btn33.TabIndex = 8;
+            this.btn33.Text = "   ";
+            this.btn33.UseMnemonic = false;
+            this.btn33.UseVisualStyleBackColor = false;
+            this.btn33.Click += new System.EventHandler(this.btn33_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.resetToolStripMenuItem,
+            this.restartToolStripMenuItem1,
+            this.closeToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(264, 38);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // resetToolStripMenuItem
+            // 
+            this.resetToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 16F);
+            this.resetToolStripMenuItem.Name = "resetToolStripMenuItem";
+            this.resetToolStripMenuItem.Size = new System.Drawing.Size(77, 34);
+            this.resetToolStripMenuItem.Text = "Reset";
+            this.resetToolStripMenuItem.Click += new System.EventHandler(this.resetToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 16F);
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(78, 34);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // lblwinner
+            // 
+            this.lblwinner.AutoSize = true;
+            this.lblwinner.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.lblwinner.Location = new System.Drawing.Point(12, 38);
+            this.lblwinner.Name = "lblwinner";
+            this.lblwinner.Size = new System.Drawing.Size(25, 24);
+            this.lblwinner.TabIndex = 10;
+            this.lblwinner.Text = "   ";
+            this.lblwinner.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblp1
+            // 
+            this.lblp1.AutoSize = true;
+            this.lblp1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.lblp1.Location = new System.Drawing.Point(12, 62);
+            this.lblp1.Name = "lblp1";
+            this.lblp1.Size = new System.Drawing.Size(77, 24);
+            this.lblp1.TabIndex = 11;
+            this.lblp1.Text = "Player 1";
+            this.lblp1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblp2
+            // 
+            this.lblp2.AutoSize = true;
+            this.lblp2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.lblp2.Location = new System.Drawing.Point(95, 62);
+            this.lblp2.Name = "lblp2";
+            this.lblp2.Size = new System.Drawing.Size(77, 24);
+            this.lblp2.TabIndex = 12;
+            this.lblp2.Text = "Player 2";
+            this.lblp2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblwp1
+            // 
+            this.lblwp1.AutoSize = true;
+            this.lblwp1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.lblwp1.Location = new System.Drawing.Point(36, 86);
+            this.lblwp1.Name = "lblwp1";
+            this.lblwp1.Size = new System.Drawing.Size(20, 24);
+            this.lblwp1.TabIndex = 13;
+            this.lblwp1.Text = "0";
+            this.lblwp1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblwp2
+            // 
+            this.lblwp2.AutoSize = true;
+            this.lblwp2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.lblwp2.Location = new System.Drawing.Point(119, 86);
+            this.lblwp2.Name = "lblwp2";
+            this.lblwp2.Size = new System.Drawing.Size(20, 24);
+            this.lblwp2.TabIndex = 14;
+            this.lblwp2.Text = "0";
+            this.lblwp2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // restartToolStripMenuItem1
+            // 
+            this.restartToolStripMenuItem1.Font = new System.Drawing.Font("Segoe UI", 16F);
+            this.restartToolStripMenuItem1.Name = "restartToolStripMenuItem1";
+            this.restartToolStripMenuItem1.Size = new System.Drawing.Size(91, 34);
+            this.restartToolStripMenuItem1.Text = "Restart";
+            this.restartToolStripMenuItem1.Click += new System.EventHandler(this.restartToolStripMenuItem1_Click);
+            // 
+            // Game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(264, 322);
+            this.Controls.Add(this.lblwp2);
+            this.Controls.Add(this.lblwp1);
+            this.Controls.Add(this.lblp2);
+            this.Controls.Add(this.lblp1);
+            this.Controls.Add(this.lblwinner);
+            this.Controls.Add(this.btn33);
+            this.Controls.Add(this.btn32);
+            this.Controls.Add(this.btn31);
+            this.Controls.Add(this.btn23);
+            this.Controls.Add(this.btn22);
+            this.Controls.Add(this.btn21);
+            this.Controls.Add(this.btn13);
+            this.Controls.Add(this.btn12);
+            this.Controls.Add(this.btn11);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Game";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn11;
+        private System.Windows.Forms.Button btn12;
+        private System.Windows.Forms.Button btn13;
+        private System.Windows.Forms.Button btn21;
+        private System.Windows.Forms.Button btn22;
+        private System.Windows.Forms.Button btn23;
+        private System.Windows.Forms.Button btn31;
+        private System.Windows.Forms.Button btn32;
+        private System.Windows.Forms.Button btn33;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem resetToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.Label lblwinner;
+        private System.Windows.Forms.Label lblp1;
+        private System.Windows.Forms.Label lblp2;
+        private System.Windows.Forms.Label lblwp1;
+        private System.Windows.Forms.Label lblwp2;
+        private System.Windows.Forms.ToolStripMenuItem restartToolStripMenuItem1;
+    }
+}
+
---
+++ Game.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tic_Tac_Toe_Game
+{
+    public partial class Game : Form
+    {
+        bool player1 = true;
+        bool locked = false;
+        int winner = 0;
+        int owns11 = 0;
+        int owns12 = 0;
+        int owns13 = 0;
+        int owns21 = 0;
+        int owns22 = 0;
+        int owns23 = 0;
+        int owns31 = 0;
+        int owns32 = 0;
+        int owns33 = 0;
+
+        public Game()
+        {
+            InitializeComponent();
+        }
+
+        private int winCondition() 
+        {
+            int winner = 0;
+            
+            if (
+                ((owns11 == 1) & (owns12 == 1) & (owns13 == 1)) |
+                ((owns21 == 1) & (owns22 == 1) & (owns23 == 1)) |
+                ((owns31 == 1) & (owns32 == 1) & (owns33 == 1)) |
+                ((owns11 == 1) & (owns21 == 1) & (owns31 == 1)) |
+                ((owns12 == 1) & (owns22 == 1) & (owns32 == 1)) |
+                ((owns13 == 1) & (owns23 == 1) & (owns33 == 1)) |
+                ((owns11 == 1) & (owns22 == 1) & (owns33 == 1)) |
+                ((owns31 == 1) & (owns22 == 1) & (owns13 == 1))                
+               )
+            {
+                locked = true;
+                winner = 1;
+                lblwinner.Text = "The Winner is Player 1";
+            }
+
+            if (
+                ((owns11 == 2) & (owns12 == 2) & (owns13 == 2)) |
+                ((owns21 == 2) & (owns22 == 2) & (owns23 == 2)) |
+                ((owns31 == 2) & (owns32 == 2) & (owns33 == 2)) |
+                ((owns11 == 2) & (owns21 == 2) & (owns31 == 2)) |
+                ((owns12 == 2) & (owns22 == 2) & (owns32 == 2)) |
+                ((owns13 == 2) & (owns23 == 2) & (owns33 == 2)) |
+                ((owns11 == 2) & (owns22 == 2) & (owns33 == 2)) |
+                ((owns31 == 2) & (owns22 == 2) & (owns13 == 2))
+               )
+            {
+                locked = true;
+                winner = 2;
+                lblwinner.Text = "The Winner is Player 2";
+            }
+
+            if (((!(owns11 == 0)) & (!(owns12 == 0)) & (!(owns13 == 0)) & (!(owns21 == 0)) & (!(owns22 == 0)) & (!(owns23 == 0)) & (!(owns31 == 0)) & (!(owns32 == 0)) & (!(owns33 == 0))) & (locked == false))
+            {
+                locked = true;
+                winner = 3;
+                lblwinner.Text = "No Winners";
+            }
+            return winner;
+        }
+
+        private void resetToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            winner = winCondition();
+
+            if (winner == 1)
+            {
+                int winsp1 = Convert.ToInt32(lblwp1.Text);
+                winsp1++;
+                lblwp1.Text = Convert.ToString(winsp1);
+            }
+
+            if (winner == 2)
+            {
+                int winsp2 = Convert.ToInt32(lblwp2.Text);
+                winsp2++;
+                lblwp2.Text = Convert.ToString(winsp2);
+            }
+
+            owns11 = 0;
+            owns12 = 0;
+            owns13 = 0;
+            owns21 = 0;
+            owns22 = 0;
+            owns23 = 0;
+            owns31 = 0;
+            owns32 = 0;
+            owns33 = 0;
+
+            btn11.Text = "";
+            btn12.Text = "";
+            btn13.Text = "";
+            btn21.Text = "";
+            btn22.Text = "";
+            btn23.Text = "";
+            btn31.Text = "";
+            btn32.Text = "";
+            btn33.Text = "";
+
+            player1 = true;
+            locked = false;
+            winner = 0;
+            lblwinner.Text = "";
+        }
+
+        private void restartToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            resetToolStripMenuItem_Click(sender, e);
+            lblwp1.Text = Convert.ToString("0");
+            lblwp2.Text = Convert.ToString("0");
+        }
+
+        private void btn11_Click(object sender, EventArgs e)
+        {
+            if ((owns11 == 0) & (locked == false))
+            {
+                if (player1 == true)
+                {
+                    btn11.Text = "x";
+                    player1 = false;
+                    owns11 = 1;
+                }
+                else
+                {
+                    btn11.Text = "o";
+                    player1 = true;
+                    owns11 = 2;
+                }
+            }
+            winCondition();
+        }
+
+        private void btn12_Click(object sender, EventArgs e)
+        {
+            if ((owns12 == 0) & (locked == false))
+            {
+                if (player1 == true)
+                {
+                    btn12.Text = "x";
+                    player1 = false;
+                    owns12 = 1;
+                }
+                else
+                {
+                    btn12.Text = "o";
+                    player1 = true;
+                    owns12 = 2;
+                }
+            }
+            winCondition();
+        }
+
+        private void btn13_Click(object sender, EventArgs e)
+        {
+            if ((owns13 == 0) & (locked == false))
+            {
+                if (player1 == true)
+                {
+                    btn13.Text = "x";
+                    player1 = false;
+                    owns13 = 1;
+                }
+                else
+                {
+                    btn13.Text = "o";
+                    player1 = true;
+                    owns13 = 2;
+                }
+            }
+            winCondition();
+        }
+
+        private void btn21_Click(object sender, EventArgs e)
+        {
+            if ((owns21 == 0) &(locked == false))
+            {
+                if (player1 == true)
+                {
+                    btn21.Text = "x";
+                    player1 = false;
+                    owns21 = 1;
+                }
+                else
+                {
+                    btn21.Text = "o";
+                    player1 = true;
+                    owns21 = 2;
+                }
+            }
+            winCondition();
+        }
+
+        private void btn22_Click(object sender, EventArgs e)
+        {
+            if ((owns22 == 0) & (locked == false))
+            {
+                if (player1 == true)
+                {
+                    btn22.Text = "x";
+                    player1 = false;
+                    owns22 = 1;
+                }
+                else
+                {
+                    btn22.Text = "o";
+                    player1 = true;
+                    owns22 = 2;
+                }
+            }
+            winCondition();
+        }
+
+        private void btn23_Click(object sender, EventArgs e)
+        {
+            if ((owns23 == 0) & (locked == false))
+            {
+                if (player1 == true)
+                {
+                    btn23.Text = "x";
+                    player1 = false;
+                    owns23 = 1;
+                }
+                else
+                {
+                    btn23.Text = "o";
+                    player1 = true;
+                    owns23 = 2;
+                }
+            }
+            winCondition();
+        }
+
+        private void btn31_Click(object sender, EventArgs e)
+        {
+            if ((owns31 == 0) & (locked == false))
+            {
+                if (player1 == true)
+                {
+                    btn31.Text = "x";
+                    player1 = false;
+                    owns31 = 1;
+                }
+                else
+                {
+                    btn31.Text = "o";
+                    player1 = true;
+                    owns31 = 2;
+                }
+            }
+            winCondition();
+        }
+
+        private void btn32_Click(object sender, EventArgs e)
+        {
+            if ((owns32 == 0) & (locked == false))
+            {
+                if (player1 == true)
+                {
+                    btn32.Text = "x";
+                    player1 = false;
+                    owns32 = 1;
+                }
+                else
+                {
+                    btn32.Text = "o";
+                    player1 = true;
+                    owns32 = 2;
+                }
+            }
+            winCondition();
+        }
+
+        private void btn33_Click(object sender, EventArgs e)
+        {
+            if ((owns33 == 0) & (locked == false))
+            {
+                if (player1 == true)
+                {
+                    btn33.Text = "x";
+                    player1 = false;
+                    owns33 = 1;
+                }
+                else
+                {
+                    btn33.Text = "o";
+                    player1 = true;
+                    owns33 = 2;
+                }
+            }
+            winCondition();
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
